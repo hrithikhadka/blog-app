784d20649094010c22c24c0498eb9777da8aad2f
Hrithik Khadka <hritik.khadka@centria.fi>
2023-02-24 13:44:16 +0200

    node and mongo db connection

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-app.use(cors());
+connectDB();
+
 app.use(express.json());
 
 app.use("/posts", (req, res) => {
   console.log("main url");
 });
-app.listen("5000", () => {
-  console.log("Backend is running");
-});
+
+app.listen(port, () => console.log(`server started on port ${port}`));
