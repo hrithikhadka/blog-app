fa2e7d8fcce495b3cf4a11390f1276b99bf208da
Hrithik Khadka <hritik.khadka@centria.fi>
2023-03-01 21:48:45 +0200

    change heading

--- frontend/src/Title.jsx
+++ frontend/src/Title.jsx
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <>
       <div className="before-post">
-        <h1>Latest</h1>
+        <h1>All Posts</h1>
         <p>A blog created using MERN Stack</p>
       </div>
     </>
