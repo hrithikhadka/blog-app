2f610f9b7ef0dde7f98e4ed2d31b5fda48e4ef02
Hrithik Khadka <hritik.khadka@centria.fi>
2023-03-12 22:24:00 +0200

    navigate to homepage after successful login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   const login = async (e) => {
     e.preventDefault();
@@ -15,7 +17,15 @@ const LoginPage = () => {
       credentials: "include",
     });
     // console.log(rsp);
+    if (rsp.ok) {
+      setRedirect(true);
+    } else {
+      alert("Wrong Credentials!");
+    }
   };
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
   return (
     <form className="login" onSubmit={login}>
       <h1>Login</h1>
