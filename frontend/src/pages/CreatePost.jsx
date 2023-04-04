13b0194740f1a92e8ccf2c39817cb2381033880a
Hrithik Khadka <hritik.khadka@centria.fi>
2023-04-04 23:19:31 +0300

    add modules and formats in editor

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,15 +1,49 @@
 import React from "react";
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
   return (
     <main>
       <form action="">
         <input type="title" placeholder={"Title"} />
         <input type="summary" placeholder={"Summary"} />
         <input type="file" />
-        <ReactQuill />
+        <ReactQuill value={content} modules={modules} formats={formats} />
         <button style={{ marginTop: ".5rem", backgroundColor: "#222" }}>
           Publish
         </button>
