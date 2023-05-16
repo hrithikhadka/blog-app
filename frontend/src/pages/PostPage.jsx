0424366763ac5f3c95e571f1c670204dd721c3e2
Hrithik Khadka <hritik.khadka@centria.fi>
2023-05-16 16:56:04 +0300

    add edit button that directs to editpost page

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
 import { format } from "date-fns";
+import { UserContext } from "../UserContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 const PostPage = () => {
   const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
   const { id } = useParams();
   useEffect(() => {
     // console.log(params);
@@ -22,9 +26,21 @@ const PostPage = () => {
   }
   return (
     <div className="post-page">
-      <time>{format(new Date(postInfo.createdAt), "MMM d, yyyy")}</time>
       <h1>{postInfo.title}</h1>
-      <div className="author">by @{postInfo.author.username}</div>
+      <div className="authEdit">
+        <time>{format(new Date(postInfo.createdAt), "MMM d, yyyy")}</time>
+
+        <div className="author">by @{postInfo.author.username}</div>
+
+        {userInfo.id === postInfo.author._id && (
+          <div className="edit-post">
+            <Link className="btn" to={`/edit/${postInfo._id}`}>
+              Edit
+            </Link>
+          </div>
+        )}
+      </div>
+
       <div className="post-image">
         <img src={`http://localhost:5000/${postInfo.photo}`} alt="" />
       </div>
