2ed754c11b8c7fe5c2f6f9c25080477d6e901bf0
Hrithik Khadka <hritik.khadka@centria.fi>
2023-05-24 13:17:59 +0300

    fix logout route and clean-up

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
+const port = 5000;
+
+//route handlers
 const loginRoute = require("./routes/login");
 const registerRoute = require("./routes/Register");
 const postRoute = require("./routes/post");
 const profileRoute = require("./routes/profile");
 const postsRoute = require("./routes/posts");
-// const logoutRoute = require("./routes/logout");
+const logoutRoute = require("./routes/logout");
 
-const port = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
+//remove cors error
 app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" }));
 
 mongoose.set("strictQuery", true);
@@ -24,21 +27,15 @@ mongoose.connect(
   "mongodb+srv://blog:blog@cluster0.ny7sbff.mongodb.net/?retryWrites=true&w=majority"
 );
 
+//configuring routes
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 app.use("/post", postRoute);
 app.use("/profile", profileRoute);
 app.use("/posts", postsRoute);
-// app.use("/logout", logoutRoute);
-
-//logout user
-app.post("/logout", (req, res) => {
-  // res.clearCookie("token");
-  res.cookie("token", "");
-  res.send("cookie cleared");
-});
+app.use("/logout", logoutRoute);
 
 app.get("/", (req, res) => {
-  res.json("hello");
+  res.send("hello");
 });
 app.listen(port, () => console.log(`server started on port ${port}`));
